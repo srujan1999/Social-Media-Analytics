ea6097d67e4a0c9e3593411459c6efa70cb3f458
srujan1999 <mylaramsrujankumar@msitprogram.net>
2022-01-05 14:34:02 +0530

    Parse Message

--- hw6_social.py
+++ hw6_social.py
@@ -83,7 +83,20 @@ Parameters: str
 Returns: list of strs
 '''
 def findHashtags(message):
-    return
+    # import re
+    # return re.findall(r'#\w+', message)
+    new=message.split("#")
+    s=""
+    l1=[]
+    for i in new[1:]:
+        for j in i:
+            if j not in endChars:
+                s+=j
+            else:
+                break
+        l1.append('#'+s)
+        s=""
+    return l1
 
 
 '''
@@ -284,9 +297,10 @@ if __name__ == "__main__":
     #test.week1Tests()
     print("\n" + "#"*15 + " WEEK 1 OUTPUT " + "#" * 15 + "\n")
     #test.runWeek1()
-    test.testParseName()
-    test.testParsePosition()
-    test.testParseState()
+    # test.testParseName()
+    # test.testParsePosition()
+    # test.testParseState()
+    test.testFindHashtags()
     ## Uncomment these for Week 2 ##
     """print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
     test.week2Tests()
